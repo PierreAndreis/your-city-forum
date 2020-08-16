1d1134ad5b3af001b5fed729148e0fa1c0127e10
Dtesch9 <alucard.s8123@gmail.com>
2020-08-16 19:15:01 -0300

    feat: Fetch Topics from API

--- web/src/components/TopicItem/index.tsx
+++ web/src/components/TopicItem/index.tsx
@@ -4,18 +4,28 @@ import { FiMessageSquare } from 'react-icons/fi';
 
 import { Container } from './styles';
 
-const TopicItem: React.FC = () => {
+interface Opinion {
+  id: number;
+  title: string;
+  upvotes_count: number;
+}
+
+type TopicItemProps = {
+  opinion: Opinion;
+};
+
+const TopicItem: React.FC<TopicItemProps> = ({ opinion }) => {
   return (
     <Container>
       <Link to="/topic">
         <section>
           <FiMessageSquare />
-          <h2>Buraco na rua Chile</h2>
+          <h2>{opinion.title}</h2>
         </section>
 
         <aside>
           <strong>
-            Votos <span>0</span>
+            Votos <span>{opinion.upvotes_count}</span>
           </strong>
         </aside>
       </Link>
--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,6 +1,9 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { Form } from '@unform/web';
 import { FiPlus } from 'react-icons/fi';
+import { toast } from 'react-toastify';
+
+import api from '../../services/api';
 
 import {
   Header,
@@ -18,12 +21,42 @@ import {
   TopicsList,
 } from './styles';
 
+interface Opinion {
+  id: number;
+  title: string;
+  upvotes_count: number;
+}
+
+interface ApiResponse {
+  opinions: Opinion[];
+}
+
 interface FormData {
   filter: string;
 }
 
 const Dashboard: React.FC = () => {
   const [toggleTopicVisible, setToggleTopicVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [opinions, setOpinions] = useState<Opinion[]>([]);
+
+  useEffect(() => {
+    async function loadData(): Promise<void> {
+      try {
+        setLoading(true);
+
+        const response = await api.get<ApiResponse>('/opinions');
+
+        setOpinions(response.data.opinions);
+      } catch (err) {
+        toast('Erro inesperado, relogue e tente novamente', { type: 'error' });
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    loadData();
+  }, []);
 
   const handleToggleTopicModal = useCallback(() => {
     setToggleTopicVisible(state => !state);
@@ -51,20 +84,19 @@ const Dashboard: React.FC = () => {
               </ActionsContainer>
             </Form>
 
-            {/* <Loading
-            containerStyle={{ marginTop: '-200px' }}
-            size={70}
-            color="#00FF66"
-          /> */}
-
-            <TopicsList>
-              <TopicItem />
-
-              <TopicItem />
-              <TopicItem />
-              <TopicItem />
-              <TopicItem />
-            </TopicsList>
+            {loading ? (
+              <Loading
+                containerStyle={{ marginTop: '-200px' }}
+                size={70}
+                color="#00FF66"
+              />
+            ) : (
+              <TopicsList>
+                {opinions.map(opinion => (
+                  <TopicItem opinion={opinion} />
+                ))}
+              </TopicsList>
+            )}
           </ContentContainer>
         </Wrapper>
       </Container>
